635e7b971e650e6495141a5dbecd719425728674
Patrick Del Conte <patrick.delconte@umb.ch>
2018-08-10 23:40:16 +0200

    add spider monster

---
+++ Assets/Scripts/Monsters/MonsterAttributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Monsters
+{
+    [Serializable]
+    public struct MonsterAttributes
+    {
+        public int Health;
+        public int Attackpower;
+        public int Speed;
+        
+        public int EnergyValue;
+    }
+}
