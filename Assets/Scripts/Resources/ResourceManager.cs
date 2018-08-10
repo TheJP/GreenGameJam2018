986237731bcd73dfa7b8fa776589fcad95112caa
Florian Graf <floriangraf@gmx.ch>
2018-08-11 01:19:23 +0200

    Added storage system

--- Assets/Scripts/Resources/ResourceManager.cs
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -1,89 +1,147 @@
 ﻿using System;
+using System.Linq;
 using Boo.Lang;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 namespace Resources
 {
     public class ResourceManager : MonoBehaviour
     {
         [SerializeField]
-        [Tooltip("The amount of energy available")]
-        private Energy energyStorage;
+        [Tooltip("The energy base storage")]
+        private EnergyStorage energyBaseStorage;
         
         [SerializeField]
-        [Tooltip("The amount of oxygen available")]
-        private Oxygen oxygenStorage;
+        [Tooltip("The oxygen base storage")]
+        private OxygenStorage oxygenBaseStorage;
 
         [SerializeField]
-        [Tooltip("The amount of construction material available")]
-        private ConstructionMaterial constructionMaterialStorage;
+        [Tooltip("The construction material base storage")]
+        private ConstructionMaterialStorage constructionMaterialBaseStorage;
 
         private readonly List<EnergySink> energySinks;
         private readonly List<EnergySource> energySources;
+        private readonly List<EnergyStorage> energyStorages;
+        
         private readonly List<OxygenSink> oxygenSinks;
         private readonly List<OxygenSource> oxygenSources;
+        private readonly List<OxygenStorage> oxygenStorages;
+        
         private readonly List<ConstructionMaterialSink> constructionMaterialSinks;
         private readonly List<ConstructionMaterialSource> constructionMaterialSources;
+        private readonly List<ConstructionMaterialStorage> constructionMaterialStorages;
 
-        public Energy EnergyAvailable => energyStorage;
-        public Oxygen OxygenAvailable => oxygenStorage;
-        public ConstructionMaterial ConstructionMaterialAvailable => constructionMaterialStorage;
+        public EnergyStorage EnergyBaseStorage => energyBaseStorage;
+        public OxygenStorage OxygenBaseStorage => oxygenBaseStorage;
+        public ConstructionMaterialStorage ConstructionMaterialBaseStorage => constructionMaterialBaseStorage;
+
+        public Energy EnergyAvailable => energyStorages.Aggregate(energyBaseStorage.CurrentValue, (a, s) => a + s.CurrentValue);
+        public Oxygen OxygenAvailable => oxygenStorages.Aggregate(oxygenBaseStorage.CurrentValue, (a, s) => a + s.CurrentValue);
+        public ConstructionMaterial ConstructionMaterialAvailable => constructionMaterialStorages.Aggregate(constructionMaterialBaseStorage.CurrentValue, (a, s) => a + s.CurrentValue);
 
         public ResourceManager()
         {
             energySinks = new List<EnergySink>();
             energySources = new List<EnergySource>();
+            energyStorages = new List<EnergyStorage>();
+            
             oxygenSinks = new List<OxygenSink>();
             oxygenSources = new List<OxygenSource>();
+            oxygenStorages = new List<OxygenStorage>();
+            
             constructionMaterialSinks = new List<ConstructionMaterialSink>();
             constructionMaterialSources = new List<ConstructionMaterialSource>();
+            constructionMaterialStorages = new List<ConstructionMaterialStorage>();
         }
 
         public void Store(Energy energy)
         {
-            energyStorage += energy;
+            energy = energyBaseStorage.Store(energy);
+
+            using (var storages = energyStorages.GetEnumerator())
+            {
+                while (energy > Energy.Zero && storages.MoveNext())
+                {
+                    Debug.Assert(storages.Current != null, "storages.Current != null");
+                    energy = storages.Current.Store(energy);
+                }
+            }
         }
         
         public void Store(Oxygen oxygen)
         {
-            oxygenStorage += oxygen;
+            oxygen = oxygenBaseStorage.Store(oxygen);
+
+            using (var storages = oxygenStorages.GetEnumerator())
+            {
+                while (oxygen > Oxygen.Zero && storages.MoveNext())
+                {
+                    Debug.Assert(storages.Current != null, "storages.Current != null");
+                    oxygen = storages.Current.Store(oxygen);
+                }
+            }
         }
 
         public void Store(ConstructionMaterial material)
         {
-            constructionMaterialStorage += material;
+            material = constructionMaterialBaseStorage.Store(material);
+
+            using (var storages = constructionMaterialStorages.GetEnumerator())
+            {
+                while (material > ConstructionMaterial.Zero && storages.MoveNext())
+                {
+                    Debug.Assert(storages.Current != null, "storages.Current != null");
+                    material = storages.Current.Store(material);
+                }
+            }
         }
         
         public bool TryConsume(Energy energy)
         {
-            if (energy > energyStorage)
+            if (energy > EnergyAvailable)
             {
                 return false;
             }
 
-            energyStorage -= energy;
+            energy = energyBaseStorage.Consume(energy);
+            foreach (var storage in energyStorages)
+            {
+                energy = storage.Consume(energy);
+            }
+            
             return true;
         }
         
         public bool TryConsume(Oxygen oxygen)
         {
-            if (oxygen > oxygenStorage)
+            if (oxygen > OxygenAvailable)
             {
                 return false;
             }
 
-            oxygenStorage -= oxygen;
+            oxygen = oxygenBaseStorage.Consume(oxygen);
+            foreach (var storage in oxygenStorages)
+            {
+                oxygen = storage.Consume(oxygen);
+            }
+            
             return true;
         }
 
         public bool TryConsume(ConstructionMaterial material)
         {
-            if (material > constructionMaterialStorage)
+            if (material > ConstructionMaterialAvailable)
             {
                 return false;
             }
 
-            constructionMaterialStorage -= material;
+            material = constructionMaterialBaseStorage.Consume(material);
+            foreach (var storage in constructionMaterialStorages)
+            {
+                material = storage.Consume(material);
+            }
+            
             return true;
         }
 
@@ -175,6 +233,51 @@ namespace Resources
         public void RemoveSink(ConstructionMaterialSink sink)
         {
             constructionMaterialSinks.Remove(sink);
+        }
+
+        public void AddStorage(EnergyStorage storage)
+        {
+            if (storage == null)
+            {
+                throw new ArgumentNullException(nameof(storage));
+            }
+
+            energyStorages.Add(storage);
+        }
+
+        public void AddStorage(OxygenStorage storage)
+        {
+            if (storage == null)
+            {
+                throw new ArgumentNullException(nameof(storage));
+            }
+
+            oxygenStorages.Add(storage);
+        }
+
+        public void AddStorage(ConstructionMaterialStorage storage)
+        {
+            if (storage == null)
+            {
+                throw new ArgumentNullException(nameof(storage));
+            }
+
+            constructionMaterialStorages.Add(storage);
+        }
+
+        public void RemoveStorage(EnergyStorage storage)
+        {
+            energyStorages.Remove(storage);
+        }
+
+        public void RemoveStorage(OxygenStorage storage)
+        {
+            oxygenStorages.Remove(storage);
+        }
+
+        public void RemoveStorage(ConstructionMaterialStorage storage)
+        {
+            constructionMaterialStorages.Remove(storage);
         }
 
         private void FixedUpdate()
