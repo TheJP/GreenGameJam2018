89a1d81f929366804e8fec4597308385ea2f7fe9
Stefan Winterberger <stefanwinterb88@hotmail.com>
2018-08-10 22:11:29 +0200

    Added Draft of a Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private Sprite sprite;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private int maxOxygen;
+    private int currentOxygen;
+    //Phobie
+    //Equipped Weapon
+    //Inventory
+
+    // Use this for initialization
+    void Start()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.spriteRenderer.sprite = sprite;
+
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            this.animator.enabled = true;
+        }
+        else
+        {
+            this.animator.enabled = false;
+        }
+    }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 startPosition;
+    private readonly float maxVelocityVertical = 1.0F;
+    private readonly float maxVelocityHorizontal = 1.0F;
+
+    [SerializeField]
+    private Vector2 currentPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.currentPosition = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (IsVerticalMovementAllowed())
+        {
+            //Flip Sprite Direction
+            //StartAnimation
+            float translationVertical = Input.GetAxis("Vertical") * maxVelocityVertical;
+            currentPosition.x = currentPosition.x + translationVertical;
+        }
+
+        if (IsHorizontalMovementAllowed())
+        {
+            //StartAnimation
+            float translationHorizontal = Input.GetAxis("Horizontal") * maxVelocityHorizontal;
+            currentPosition.y = currentPosition.y + translationHorizontal;
+        }
+
+        //StopAnimation
+
+        //Jump
+    }
+
+    private bool IsHorizontalMovementAllowed()
+    {
+        return true;
+    }
+
+    private bool IsVerticalMovementAllowed()
+    {
+        return true;
+    }
+
+    private bool IsJumpingAllowed()
+    {
+        return true;
+    }
+}
