9c91ce0a42178a37119a847f9f8b79872d7db53f
Florian Graf <floriangraf@gmx.ch>
2018-08-11 07:21:06 +0200

    Added energy storage (some call it a battery, but that is lame)

---
+++ Assets/Scripts/Placeables/EnergyStorageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Resources;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(EnergyStorage))]
+public class EnergyStorageController : Placeable
+{
+	private const string AnimatorPercent = "Percent";
+	
+	private Animator animator;
+	private EnergyStorage energyStorage;
+
+	private ResourceManager resourceManager;
+	
+	protected override void Start ()
+	{
+		base.Start();
+		
+		animator = GetComponent<Animator>();
+		energyStorage = GetComponent<EnergyStorage>();
+
+		resourceManager = FindObjectOfType<ResourceManager>();
+		resourceManager.AddStorage(energyStorage);
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+		
+		resourceManager.RemoveStorage(energyStorage);
+	}
+
+	private void Update ()
+	{
+		var energyPercent = energyStorage.CurrentValue / energyStorage.Capacity;
+		animator.SetFloat(AnimatorPercent, energyPercent);
+	}
+}
