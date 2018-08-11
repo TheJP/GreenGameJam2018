1cfd7ced4b56b05eb28e9aa80ce0ef751dd11128
Florian Graf <floriangraf@gmx.ch>
2018-08-11 04:45:49 +0200

    Solar array can be on/off and display that state

--- Assets/Scripts/Placeables/SolarArray.cs
+++ Assets/Scripts/Placeables/SolarArray.cs
@@ -6,18 +6,42 @@ public class SolarArray : MonoBehaviour, EnergySource
     #pragma warning disable 0649
     [SerializeField]
     private Energy energyPerSecond;
+
+    [SerializeField]
+    private Sprite solarOffSprite;
+    
+    [SerializeField]
+    private Sprite solarOnSprite;
+    
+    [SerializeField]
+    private bool isSolarOn;
     #pragma warning restore 0649
+
+    public bool IsSolarOn
+    {
+        get { return isSolarOn; }
+        set { isSolarOn = value; }
+    }
 
     private ResourceManager resourceManager;
     private EnergyStorage energyStorage;
 
+    private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
         resourceManager = FindObjectOfType<ResourceManager>();
         energyStorage = GetComponent<EnergyStorage>();
         
         resourceManager.AddSource(this);
         resourceManager.AddStorage(energyStorage);
+    }
+
+    private void Update()
+    {
+        spriteRenderer.sprite = IsSolarOn ? solarOnSprite : solarOffSprite;
     }
 
     private void OnDestroy()
@@ -28,6 +52,9 @@ public class SolarArray : MonoBehaviour, EnergySource
 
     public void ProduceEnergy(ResourceManager manager)
     {
-        manager.Store(energyPerSecond * Time.deltaTime);
+        if (IsSolarOn)
+        {
+            manager.Store(energyPerSecond * Time.deltaTime);
+        }
     }
 }
