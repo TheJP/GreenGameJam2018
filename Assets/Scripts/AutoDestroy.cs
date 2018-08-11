107af731dcfb7fe48ce1b1da8d0dba74f8b13235
Florian Graf <floriangraf@gmx.ch>
2018-08-11 14:01:54 +0200

    Added turret

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+	[SerializeField]
+	private float destroyAfter;
+	
+	private void Start ()
+	{
+		StartCoroutine(StartSelftDestructionTimer());
+	}
+
+	private IEnumerator StartSelftDestructionTimer()
+	{
+		yield return new WaitForSeconds(destroyAfter);
+		
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Placeables/TurretController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using Resources;
+using UnityEngine;
+
+public class TurretController : Placeable, EnergySink
+{
+	[SerializeField]
+	[Tooltip("The barrel of the turret. Should be preconfigured in the Prefab")]
+	private GameObject barrel;
+
+	[SerializeField]
+	[Tooltip("The bullet Prefab")]
+	private GameObject bulletPrefab;
+
+	[SerializeField]
+	[Tooltip("The bullet spawn point. Should be preconfigured in the Prefab")]
+	private GameObject bulletSpawn;
+
+	[SerializeField]
+	[Tooltip("The frequency of bullets shot")]
+	private float bulletFrequency;
+
+	[SerializeField]
+	[Tooltip("The energy needed to shoot a bullet")]
+	private Energy energyPerBullet;
+	
+	[SerializeField]
+	[Tooltip("Standby energy per second")]
+	private Energy standbyEnergy;
+
+	[SerializeField]
+	[Tooltip("For testing only")]
+	private GameObject dummyTarget;
+
+	private float currentAngle;
+	private ResourceManager resourceManager;
+
+	private bool hasEnergy;
+	
+	protected override void Start()
+	{
+		base.Start();
+
+		resourceManager = FindObjectOfType<ResourceManager>();
+		resourceManager.AddSink(this);
+
+		if (dummyTarget != null)
+		{
+			AimAt(dummyTarget);
+		}
+	}
+
+	public void AimAt(GameObject target)
+	{
+		StartCoroutine(FireAt(target));
+	}
+
+	private IEnumerator FireAt(GameObject target)
+	{
+		var waitTime = 1 / bulletFrequency;
+		
+		// ReSharper disable once LoopVariableIsNeverChangedInsideLoop
+		while (target != null)
+		{
+			var fire = true;
+			var angleDiff = Vector2.SignedAngle(-barrel.transform.right, target.transform.position - transform.position);
+			if (currentAngle + angleDiff > 0)
+			{
+				fire = false;
+				angleDiff = -currentAngle;
+			}
+			else if (currentAngle + angleDiff < -180)
+			{
+				fire = false;
+				angleDiff = -180 - currentAngle;
+			}
+			
+			barrel.transform.RotateAround(transform.position, Vector3.forward, angleDiff);
+			currentAngle += angleDiff;
+			
+			if (fire && waitTime <= 0 && hasEnergy && resourceManager.TryConsume(energyPerBullet))
+			{
+				waitTime = 1 / bulletFrequency;
+				var bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, Quaternion.identity);
+				bullet.GetComponent<Rigidbody2D>().AddForce(-bulletSpawn.transform.right * 10);
+			}
+			else
+			{
+				waitTime -= Time.deltaTime;
+			}
+			
+			yield return null;
+		}
+	}
+
+	public void ConsumeEnergy(ResourceManager manager)
+	{
+		hasEnergy = manager.TryConsume(standbyEnergy * Time.fixedDeltaTime);
+	}
+}
