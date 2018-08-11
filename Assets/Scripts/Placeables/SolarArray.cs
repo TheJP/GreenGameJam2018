14dd5cf4fbd12668b56c9b6beea35d273b65cdb7
Florian Graf <floriangraf@gmx.ch>
2018-08-11 04:16:09 +0200

    SolarArray

---
+++ Assets/Scripts/Placeables/SolarArray.cs
@@ -0,0 +1,15 @@
+﻿using Resources;
+using UnityEngine;
+
+public class SolarArray : MonoBehaviour, EnergySource
+{
+    #pragma warning disable 0649
+    [SerializeField]
+    private Energy energyPerSecond;
+    #pragma warning restore 0649
+
+    public void ProduceEnergy(ResourceManager manager)
+    {
+        manager.Store(energyPerSecond * Time.deltaTime);
+    }
+}
