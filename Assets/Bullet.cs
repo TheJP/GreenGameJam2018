d20951d01852e83e24baef5b40c140ab41efab4d
Stefan Winterberger <stefanwinterb88@hotmail.com>
2018-08-12 13:46:10 +0200

    Added damage to the club.

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,13 +4,13 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("The Damge the bullet makes")]
-    private float damge;
+    private float damage = 5;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            other.gameObject.GetComponent<Health>().TakeDamage(damge);
+            other.gameObject.GetComponent<Health>().TakeDamage(damage);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Weapons/Club.cs
+++ Assets/Scripts/Weapons/Club.cs
@@ -7,14 +7,24 @@ using UnityEngine;
 
 public class Club : MonoBehaviour, IPlayerWeapon
 {
+#pragma warning disable 0649
     [SerializeField]
     private Animator animator;
 
     [SerializeField]
     private AudioSource audioSource;
 
+    [SerializeField]
+    private Collider2D damageCollider;
+#pragma warning restore 0649
+
+    [SerializeField]
+    private float damage = 20;
+
     private float originalPitch;
     private float pitchRange = 0.2f;
+    private const float ColiderResetTime = 0.5F;
+    private float coliderResetTimer;
 
     private void Awake()
     {
@@ -24,16 +34,33 @@ public class Club : MonoBehaviour, IPlayerWeapon
             audioSource.loop = false;
             audioSource.playOnAwake = false;
             originalPitch = audioSource.pitch;
-            audioSource.pitch = UnityEngine.Random.Range(originalPitch - pitchRange, originalPitch + pitchRange);                
+            audioSource.pitch = UnityEngine.Random.Range(originalPitch - pitchRange, originalPitch + pitchRange);
+        }
+    }
+
+    private void Update()
+    {
+        this.coliderResetTimer += Time.deltaTime;
+        if (coliderResetTimer > ColiderResetTime)
+        {
+            coliderResetTimer = 0.0F;
+            damageCollider.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Health>()?.TakeDamage(damage);
+            damageCollider.enabled = false;
         }
     }
 
     public void Fire()
     {
         animator.SetTrigger("beat");
-
         audioSource.Play();
-
-        //TODO: Make Damge to Mobs
+        damageCollider.enabled = true;
     }
 }
