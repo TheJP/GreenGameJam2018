7b0b2715d50c6cd79add0e5e80141b5be17efaf0
EdithBirrer <edith.birrer@hslu.ch>
2018-08-12 17:09:56 +0200

    added siren for blood moon

--- Assets/Scripts/BloodMoon.cs
+++ Assets/Scripts/BloodMoon.cs
@@ -18,11 +18,20 @@ public class BloodMoon : MonoBehaviour
 	[Tooltip("Make sure it is large enough for them to complete the planet system minigame (e.g. 30s+)")]
 	private float advancedWarningTime = 50;
 
+	[SerializeField]
+	[Tooltip("Audio to play as warning before blood moon")]
+	private AudioSource audioSourceWarning;
+
+	[SerializeField]
+	[Tooltip("Audio to play when blood moon occurs")]
+	private AudioSource audioSourceBloodMoon;
+
+	
 	private IEnumerator currentBloodMoon;
 
 	private void Start()
 	{
-		StartBloodMoonRoutine();
+		StartBloodMoonRoutine();		
 	}
 
 	private IEnumerator NextBloodMoonEvent(float secondsUntil)
@@ -32,10 +41,12 @@ public class BloodMoon : MonoBehaviour
 		// Action station, action station
 		// Blood moon might be happening soon, give out alerts, man the stations
 		BloodMoonAdvancedWarning?.Invoke(advancedWarningTime);
+		audioSourceWarning.Play();
 		
 		yield return new WaitForSeconds(advancedWarningTime);
 		
 		// Too late, blood moon is now happening
+		audioSourceBloodMoon.Play();
 		BloodMoonAlert?.Invoke();
 		
 		StartBloodMoonRoutine();
