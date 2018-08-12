3a1158e65ad6df8b7222ba175bb9660c8f7a3b4b
Patrick Del Conte <patrick.delconte@umb.ch>
2018-08-12 16:49:49 +0200

    arachnid movement uses attribute from monster

--- Assets/Scripts/Monsters/ArachnidMonsterMovement.cs
+++ Assets/Scripts/Monsters/ArachnidMonsterMovement.cs
@@ -8,14 +8,13 @@ namespace Monsters
     {
         private MoveDirection _lastHorizontal = MoveDirection.Left;
         private Rigidbody2D _rigidbody2D;
-        private MonsterAttributes _attributes;
+        private Monster _monster;
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _attributes = GetComponent<MonsterAttributes>();
+            _monster = GetComponent<Monster>();
         }
-
 
         public override void MoveTowards(AttackableBuilding target)
         {
@@ -30,7 +29,7 @@ namespace Monsters
                 transform.localScale = new Vector3(Math.Abs(transform.localScale.x) * -1, transform.localScale.y,
                     transform.localScale.z);
             }
-            else if (target.transform.position.x > transform.position.x + _attributes.Range)
+            else if (target.transform.position.x > transform.position.x + _monster.Attributes.Range)
             {
                 transform.localScale = new Vector3(Math.Abs(transform.localScale.x), transform.localScale.y,
                     transform.localScale.z);
@@ -41,16 +40,17 @@ namespace Monsters
                 case MoveDirection.Left:
                 case MoveDirection.Right:
                     var forceDir = direction == MoveDirection.Left ? Vector3.left : Vector3.right;
-                    _rigidbody2D.AddForce(forceDir * _attributes.MoveForce);
-                    if (Mathf.Abs(_rigidbody2D.velocity.x) > _attributes.MaxSpeed)
+                    _rigidbody2D.AddForce(forceDir * _monster.Attributes.MoveForce);
+                    if (Mathf.Abs(_rigidbody2D.velocity.x) > _monster.Attributes.MaxSpeed)
                     {
-                        _rigidbody2D.velocity = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x) * _attributes.MaxSpeed,
+                        _rigidbody2D.velocity = new Vector2(
+                            Mathf.Sign(_rigidbody2D.velocity.x) * _monster.Attributes.MaxSpeed,
                             _rigidbody2D.velocity.y);
                     }
 
                     break;
                 case MoveDirection.Up:
-                    _rigidbody2D.AddForce(new Vector2(0, _attributes.JumpForce));
+                    _rigidbody2D.AddForce(new Vector2(0, _monster.Attributes.JumpForce));
                     break;
                 case MoveDirection.Down:
                 case MoveDirection.None:
