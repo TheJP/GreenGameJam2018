26e33d148a6b58ea83e787b244375f9770dab691
EdithBirrer <edith.birrer@hslu.ch>
2018-08-25 14:41:59 +0200

    [wip] start to add meteor movement

---
+++ Assets/Scripts/Monsters/MeteorMonsterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monsters
+{
+    public class MeteorMonsterMovement : MonsterMovement
+    {
+
+        public int speed;
+        
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+
+        public override void MoveTowards(AttackableBuilding attackableBuilding)
+        {
+            
+            throw new System.NotImplementedException();
+        }
+
+        public override bool CanOnlyFall()
+        {
+            return true;
+        }
+    }
+
+}
+
